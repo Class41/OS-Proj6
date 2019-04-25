4a98ee9ab9f9953728868cb9cde70ec0cf111153
class41 <projectclass41@gmail.com>
2019-04-25 14:35:09 -0500

    added resource display

--- oss.c
+++ oss.c
@@ -272,7 +272,7 @@ void DisplayResources()
 
 	for (i = 0; i < MEM_SIZE / PAGE_SIZE; i++)
 	{
-		printf("[0x2%x]\t%c%c%c%c%c%c%c%c\t%x\t%5i", i, BYTE_TO_BINARY(mem.mainMemory.frames[i].ref), mem.mainMemory.frames[i].currentPid);
+		printf("\n[0x2%x]\t%c%c%c%c%c%c%c%c\t%x\t%5i", i, BYTE_TO_BINARY(mem.mainMemory.frames[i].ref), mem.mainMemory.frames[i].currentPid);
 	}
 }
 
