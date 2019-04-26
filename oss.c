4e3fa8e96265ad2a402c9f250e158da6dfec5089
class41 <projectclass41@gmail.com>
2019-04-26 13:58:25 -0500

    added loop

--- oss.c
+++ oss.c
@@ -441,7 +441,7 @@ void DisplayResources()
 
 	for (i = 0; i < MEM_SIZE / PAGE_SIZE; i++)
 	{
-		printf("\n[0x%5x]\t%c%c%c%c%c%c%c%c\t%x\t%5i", i * 1000, BYTE_TO_BINARY(mem.mainMemory.frames[i].ref), mem.mainMemory.frames[i].dirty, mem.mainMemory.frames[i].currentPid);
+		printf("\n[0x%-5x]\t%c%c%c%c%c%c%c%c\t%x\t%5i", i * 1000, BYTE_TO_BINARY(mem.mainMemory.frames[i].ref), mem.mainMemory.frames[i].dirty, mem.mainMemory.frames[i].currentPid);
 	}
 }
 
